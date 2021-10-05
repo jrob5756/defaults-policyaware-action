8795a3c172c2412a764962024da3b5550e0387a7
Jason Robert <jasonrobert@microsoft.com>
2021-10-05 16:51:19 +0000

    Added request to get policy def but, commented out for now

--- src/evaluation.ts
+++ src/evaluation.ts
@@ -3,27 +3,41 @@
 import * as fs from 'fs';
 import * as core from '@actions/core';
 import { RestClient } from 'typed-rest-client/RestClient';
+import { EvaluationResponse, PolicyEvaluation } from './models';
 
 export async function evaluateTemplate(templatePath: string, accessToken: string, subscriptionId: string): Promise<void> {
   core.debug(`Reading template from ${templatePath}`);
-  const rawdata = fs.readFileSync(templatePath, 'utf8');
-  const template = JSON.parse(rawdata);
-  await evaluateResources(template.resources, accessToken, subscriptionId);
+  const template = JSON.parse(fs.readFileSync(templatePath, 'utf8'));
+
+  core.debug(`Evaluating ${template.resources.length} resources`);
+  const resp = await evaluateResources(template.resources, accessToken, subscriptionId);
+
+  core.debug(`Recieved results for ${resp.length} policy violations, retrieving policy definitions...`);
+
+  // const client = new RestClient('github', `https://management.azure.com/subscriptions/${subscriptionId}`);
+  // // eslint-disable-next-line @typescript-eslint/promise-function-async
+  // const policies = await Promise.all(resp.map(x => client.get<PolicyDefinition>(`/providers/Microsoft.Authorization/policyDefinitions/${x.policyInfo.policyDefinitionId}?api-version=2021-06-01`)));
+  // return policies.map(x => x.result).filter(notEmpty);
 }
 
-async function evaluateResources(resources: any[], accessToken: string, subscriptionId: string): Promise<void> {
+async function evaluateResources(resources: any[], accessToken: string, subscriptionId: string): Promise<PolicyEvaluation[]> {
+  let result: PolicyEvaluation[] = [];
   for (const resource of resources) {
     core.debug(`Evaluating resource ${resource.name} of type ${resource.type}`);
-    await evaluateResource(resource, accessToken, subscriptionId);
+    const evaluations = await evaluateResource(resource, accessToken, subscriptionId);
+    if (evaluations) {
+      result = [...result, ...evaluations];
+    }
   }
+  return result;
 }
 
-async function evaluateResource(resource: any, accessToken: string, subscriptionId: string): Promise<void> {
+async function evaluateResource(resource: any, accessToken: string, subscriptionId: string): Promise<PolicyEvaluation[] | undefined> {
   const url = `https://management.azure.com/subscriptions/${subscriptionId}/providers/Microsoft.PolicyInsights/checkPolicyRestrictions?api-version=2020-07-01`;
   const client = new RestClient('github');
 
   core.debug(`Calling check policy API at ${url}`);
-  const resp = await client.create(
+  const resp = await client.create<EvaluationResponse>(
     url,
     {
       pendingFields: [],
@@ -41,4 +55,10 @@ async function evaluateResource(resource: any, accessToken: string, subscription
 
   core.debug(resp.statusCode.toString());
   core.debug(JSON.stringify(resp.result));
+
+  return resp.result?.contentEvaluationResult?.policyEvaluations;
 }
+
+// function notEmpty<TValue>(value: TValue | null | undefined): value is TValue {
+//   return value !== null && value !== undefined;
+// }
---
+++ src/models.ts
@@ -0,0 +1,70 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+export interface EvaluationResponse {
+  fieldRestrictions: FieldRestriction[];
+  contentEvaluationResult: ContentEvaluationResult;
+}
+
+export interface PolicyInfo {
+  policyDefinitionId: string;
+  policySetDefinitionId: string;
+  policyDefinitionReferenceId: string;
+  policySetDefinitionName: string;
+  policySetDefinitionVersion: string;
+  policyDefinitionName: string;
+  policyDefinitionEffect: string;
+  policyAssignmentId: string;
+  policyAssignmentName: string;
+  policyAssignmentScope: string;
+}
+
+export interface EvaluatedExpression {
+  result: string;
+  expressionKind: string;
+  expression: string;
+  path: string;
+  expressionValue: string;
+  targetValue: string;
+  operator: string;
+}
+
+export interface EvaluationDetails {
+  evaluatedExpressions: EvaluatedExpression[];
+}
+
+export interface PolicyEvaluation {
+  policyInfo: PolicyInfo;
+  evaluationResult: string;
+  evaluationDetails: EvaluationDetails;
+}
+
+export interface ContentEvaluationResult {
+  policyEvaluations: PolicyEvaluation[];
+}
+
+export interface FieldRestriction {
+  field: string;
+  restrictions: Restriction[];
+}
+
+export interface Restriction {
+  result: string;
+  values: string[];
+  policy: PolicyInfo;
+}
+
+export interface PolicyDefinition {
+  id: string;
+  type: string;
+  name: string;
+  properties: PolicyDefinitionProperties;
+}
+
+export interface PolicyDefinitionProperties {
+  displayName: string;
+  description: string;
+  policyType: string;
+  mode: string;
+  metadata: any;
+  parameters: any;
+  policyRule: any;
+}
